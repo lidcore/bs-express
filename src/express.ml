07abd2cb9ed064a581b0564e85a322b0f051129d
Romain Beauxis <toots@rastageeks.org>
2018-04-23 15:09:01 -0500

    Initial commit.

---
+++ src/express.ml
@@ -0,0 +1,25 @@
+type t
+type express_module
+external express : unit -> t = "express" [@@bs.module]
+external use : t -> express_module -> unit = "" [@@bs.send]
+external cors : unit -> express_module = "cors" [@@bs.module]
+
+let init ?(useCors=true) () =
+  let app = express () in
+  if useCors then
+    use app (cors());
+  app
+
+type request = private {
+  body:    string Js.Dict.t;
+  headers: string Js.Dict.t;
+  params:  string Js.Dict.t;
+  query:   string Js.Dict.t
+} [@@bs.deriving abstract]
+
+type response
+type handler = request -> response -> unit
+
+external get  : t -> string -> handler -> unit = "" [@@bs.send]
+external post : t -> string -> handler -> unit = "" [@@bs.send]
+external put  : t -> string -> handler -> unit = "" [@@bs.send]
---
+++ src/express.mli
@@ -0,0 +1,16 @@
+type t
+
+type request = private {
+  body:    string Js.Dict.t;
+  headers: string Js.Dict.t;
+  params:  string Js.Dict.t;
+  query:   string Js.Dict.t
+} [@@bs.deriving abstract]
+
+type response
+type handler = request -> response -> unit
+
+val init : ?useCors:bool -> unit -> t
+val get  : t -> string -> handler -> unit
+val post : t -> string -> handler -> unit
+val put  : t -> string -> handler -> unit
